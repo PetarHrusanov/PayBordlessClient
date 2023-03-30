a7f1e0f0cbe016d8fe5623bf7a1b6eaf775292b4
PetarHrusanov <59975974+PetarHrusanov@users.noreply.github.com>
2023-03-30 21:34:19 +0300

    Invoice Create Logic Improved from Companies

--- src/components/companies/Companies.js
+++ src/components/companies/Companies.js
@@ -1,62 +1,76 @@
 import React, { useEffect, useState } from 'react';
 import { Company } from './Company';
-import {InvoiceCreate} from '../invoices/InvoiceCreate';
-import companyService from "../../services/companyService";
+import { InvoiceCreate } from '../invoices/InvoiceCreate';
+import companyService from '../../services/companyService';
+import Modal from '../shared/Modal';
+import SuccessMessage from '../shared/SuccessMessage';
 
 const Companies = () => {
-    const [companiesArray, setCompaniesArray] = useState([]);
-    const [showCreateInvoice, setShowCreateInvoice] = useState(false);
-    const [selectedCompanyId, setSelectedCompanyId] = useState(null);
+  const [companiesArray, setCompaniesArray] = useState([]);
+  const [showCreateInvoice, setShowCreateInvoice] = useState(false);
+  const [selectedCompanyId, setSelectedCompanyId] = useState(null);
+  const [successMessage, setSuccessMessage] = useState('');
 
-    const fetchCompanies = () => {
-        companyService
-            .getAll()
-            .then((result) => {
-                setCompaniesArray(result);
-            })
-            .catch((error) => {
-                console.error('Error fetching companies:', error);
-            });
-    };
+  const fetchCompanies = () => {
+    companyService
+      .getAll()
+      .then((result) => {
+        setCompaniesArray(result);
+      })
+      .catch((error) => {
+        console.error('Error fetching companies:', error);
+      });
+  };
 
-    useEffect(() => {
-        fetchCompanies();
-    }, []);
+  useEffect(() => {
+    fetchCompanies();
+  }, []);
 
-    const handleActionClick = (companyId) => {
-        setSelectedCompanyId(companyId);
-        setShowCreateInvoice(true);
-    };
+  const handleActionClick = (companyId) => {
+    setSelectedCompanyId(companyId);
+    setShowCreateInvoice(true);
+  };
 
-    const handleClose = () => {
-        setShowCreateInvoice(false);
-        fetchCompanies();
-    };
+  const handleClose = () => {
+    setShowCreateInvoice(false);
+    setSuccessMessage('Invoice Created');
+    fetchCompanies();
+  };
 
-    return (
-        <>
-            <div className="table-wrapper">
-                <table className="table">
-                    <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>VAT</th>
-                        <th>Owner</th>
-                        <th>Actions</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {companiesArray.map((company) => (
-                        <Company key={company.id} {...company} onActionClick={handleActionClick} />
-                    ))}
-                    </tbody>
-                </table>
-            </div>
-            {showCreateInvoice && (
-                <InvoiceCreate companyId={selectedCompanyId} onClose={handleClose} />
-            )}
-        </>
-    );
+  return (
+    <>
+      <div className="table-wrapper">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>VAT</th>
+              <th>Owner</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {companiesArray.map((company) => (
+              <Company key={company.id} {...company} onActionClick={handleActionClick} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+      {showCreateInvoice && (
+        <Modal show={showCreateInvoice} onClose={handleClose}>
+          <InvoiceCreate
+            companyId={selectedCompanyId}
+            onClose={handleClose}
+          />
+        </Modal>
+      )}
+      {successMessage && (
+              <Modal show={!!successMessage} onClose={() => setSuccessMessage(null)}>
+                <SuccessMessage message={successMessage} />
+              </Modal>
+       )}
+    </>
+  );
 };
 
 export default Companies;
